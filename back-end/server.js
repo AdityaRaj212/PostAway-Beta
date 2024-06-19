79d46a7645457670a047c830b3e69742c06f9c3d
Aditya Raj <adityaraj@pop-os.localdomain>
2024-06-19 09:54:21 +0530

    minor bug fix in server.js to serve static react files

--- back-end/server.js
+++ back-end/server.js
@@ -18,6 +18,7 @@ const server = express();
 server.use(express.urlencoded({extended:true}));
 server.use(cors());
 server.use(express.static(path.join(`${path.resolve()}`, '../public')));
+
 // console.log('Path: '+path.join(`${path.resolve()}`, '../public'));
 server.use('/public/uploads', express.static(path.join(path.resolve(), '../public/uploads')));
 // console.log(path.resolve());
@@ -34,6 +35,14 @@ server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
 server.use('/api/comments', jwtAuth, commentRouter);
 server.use('/api/likes', jwtAuth, likesRouter);
+
+// Serve static files from the React app
+server.use(express.static(path.join(path.resolve(), 'front-end/build')));
+
+// Serve React app for any unknown routes
+server.get('*', (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'front-end/build', 'index.html'));
+});
 
 server.listen(3200,()=>{
     console.log('Server is up and running at 3200');
