5a6491ef41bdadf1696526252230359632ef45d2
Aditya Raj <adityaraj@pop-os.localdomain>
2024-06-20 17:17:18 +0530

    changed mongodb config

--- back-end/config/mongoose.js
+++ back-end/config/mongoose.js
@@ -3,11 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('MongoDB URI:', process.env.MONGO_URI);
+
 // const url = "mongodb://127.0.0.1:27017/post-away";
+const url = "mongodb+srv://adityaeducation212:adityadb212@cluster0.jm0dl8u.mongodb.net/post-away?retryWrites=true&w=majority&appName=Cluster0";
+// const url = process.env.MONGO_URI;
 
 export const connectUsingMongoose = async () => {
     try{
-        const url = process.env.MONGO_URI;
         await mongoose.connect(url);
         console.log('MongoDB is connected using mongoose');
     }catch(err){
